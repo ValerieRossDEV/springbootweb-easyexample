42ab4b40129c1b2edbd04fe1bfd52005986729e7
Valerie <41258413+ValerieRossDEV@users.noreply.github.com>
2022-12-02 17:32:27 +0100

    initial commit

---
+++ src/test/java/valerie/exampleweb/springboot/ApplicationTests.java
@@ -0,0 +1,14 @@
+package valerie.exampleweb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
